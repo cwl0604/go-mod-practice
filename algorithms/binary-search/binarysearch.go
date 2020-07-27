57cefc8f6ced4a23903d19d221dd47cd968a2d6f
cwl0604 <cwl0604@localhost.localdomain>
2020-07-27 10:29:57 +0800

    binary_test

--- algorithms/binary-search/binarysearch.go
+++ algorithms/binary-search/binarysearch.go
@@ -1,15 +1,17 @@
 package binary
+
 import "fmt"
-func Rank(key int,a []int)int{
-	for lo,hi:=0,len(a)-1;lo<=hi;{
-		mid:=lo+(hi-lo)/2
+
+func Rank(key int, a []int) int {
+	for lo, hi := 0, len(a)-1; lo <= hi; {
+		mid := lo + (hi-lo)/2
 		switch {
-		case key<a[mid]:
-			hi=mid-1
-		case key>a[mid]:
-			lo=mid+1
-		default: 
-			fmt.Println("get it hahaha!")
+		case key < a[mid]:
+			hi = mid - 1
+		case key > a[mid]:
+			lo = mid + 1
+		default:
+			fmt.Println("get it hahahaha!")
 			return mid
 		}
 	}
